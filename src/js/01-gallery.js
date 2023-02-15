614586f3cc05d5aa28fe39142291f0175b8dc361
Валентина Сіра <113180300+ValentynaSira1@users.noreply.github.com>
2023-02-16 00:40:32 +0200

    виконано 1-3 завдання модуль 8

---
+++ src/js/01-gallery.js
@@ -0,0 +1,37 @@
+// Add imports above this line 
+import { galleryItems } from './gallery-items'; 
+// Change code below this line 
+import SimpleLightbox from "simplelightbox"; 
+import "simplelightbox/dist/simple-lightbox.min.css"; 
+console.log(galleryItems);  
+const modalContainer = document.querySelector('.gallery');  
+const cardsMarkup = galleryItemsImg (galleryItems);  
+  
+modalContainer.insertAdjacentHTML('beforeend', cardsMarkup);  
+modalContainer.addEventListener('click', onImgClick);  
+ 
+function galleryItemsImg (galleryItems){  
+    return galleryItems.map(({preview, original, description}) =>{  
+        return `  
+        <div class="gallery__item">  
+        <a class="gallery__link" href="${original}">  
+          <img  
+            class="gallery__image"  
+            src="${preview}"  
+            data-source="${original}"  
+            alt="${description}"  
+          />  
+        </a>  
+      </div>  
+      `;  
+    }).join('');  
+}  
+  
+function onImgClick(e) { 
+    if (e.target.nodeName !== 'IMG') { 
+        return 
+    } 
+    
+    const lightbox = new SimpleLightbox(`.gallery a`, { captionsData: `alt`, captionDelay: 250 });  
+    e.preventDefault(); 
+};
